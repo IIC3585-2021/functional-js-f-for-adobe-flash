b2a0bf0125cb32d7acfb830d6f32332978d03737
Diego Echeverría <dhecheverria@uc.cl>
2021-04-04 20:32:43 -0400

    Codigo final

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
+const read_plays = (path) => {
+  let data = fs.readFileSync('input.txt', 'utf8');
+  let plays = data.toString().split('\n');
+  return plays
+};
+
 const get_score = (throws) => {
   if (throws === 'DB') {
     return 50;
@@ -8,13 +16,16 @@ const get_score = (throws) => {
   else if (throws === null) {
     return 0;
   }
+  else if (Number.isInteger(throws)) {
+    return throws;
+  }
   else {
-    return throws.reduce((x,y) => (x*y));
+    return throws.reduce((x,y) => (parseInt(x)*parseInt(y)));
   }
 };
 
 const total_throw_score = (score_function) => (score, throws) => {
-  let throw_score = throws.reduce((x, y) => (x + score_function(y)));
+  let throw_score = throws.reduce((x, y) => (score_function(x) + score_function(y)));
   return Math.abs(score - throw_score);
 }
 
@@ -31,34 +42,37 @@ const build_matrix = (initial_function) => (players_array) => {
 
 const get_info = build_matrix(initial_score);
 
-const compare_turns = (original_turn) => (to_compare) => (original_turn !== to_compare)
+const compare_turns = (original_turn) => (to_compare) => (original_turn !== to_compare[2]);
 
 
-const readline = require('readline');
+//Intentamos pedir input en la consola mientras se corría el programa, pero no lo logramos, ya que al trabajar con recursión
+//y async al mismo tiempo, suceden cosas raras
 
-const rl = readline.createInterface(process.stdin, process.stdout);
+// const readline = require('readline');
 
-exports.question = function(q){
+// const rl = readline.createInterface(process.stdin, process.stdout);
 
-  let response;
+// exports.question = function(q){
 
-  rl.setPrompt(q);
-  rl.prompt();
-  return new Promise(( resolve , reject) => {
+//   let response;
 
-    rl.on('line', (userInput) => {
-        response = userInput;
-        rl.close();
-    });
+//   rl.setPrompt(q);
+//   rl.prompt();
+//   return new Promise(( resolve , reject) => {
 
-    rl.on('close', () => {
-        resolve(response);
-    });
+//     rl.on('line', (userInput) => {
+//         response = userInput;
+//         rl.close();
+//     });
 
-  });
-}
+//     rl.on('close', () => {
+//         resolve(response);
+//     });
 
-const get_input = exports.question;
+//   });
+// }
+
+// const get_input = exports.question;
 
 const parse_tuple = (tup) => {
   return tup.slice(1,-1).split(",").map(x => parseInt(x))
@@ -77,28 +91,36 @@ const parse_array = (tuple_parser) => (array_str) => {
 
 const parse_throw_array = parse_array(parse_tuple);
 
+const Y = f => (x => x(x))(x => f(y => x(x)(y)))
 
-const recursive_game = (score_calculator, turn_comparison, input_function, parse_function) => async (players_array) => {
-  if ((players_array.filter((x) => x[1] === 0)).length) {
-    console.log(`El jugador ${winners[0][0]} ha gclearanado la competencia!!!!`)
+
+const recursive_game = (score_calculator, turn_comparison, parse_function) => (recursive_func) => (players_array) => {
+  const winners = players_array.filter((x) => x[1] === 1);
+  if (winners.length) {
+    console.log(`El jugador ${winners[0][0]} ha ganado la competencia!!!!`)
     return;
   }
   const not_played = players_array.filter(turn_comparison(players_array[0][2]));
+  // Turno del primero
   if (!(not_played.length)) {
-    const n_l = await input_function(`Ingrese lanzamientos de ${players_array[0][0]}:\n`);
+    const n_l = "['SB', [2,20], [3,20]]";
     const lanzamientos = parse_function(n_l);
+    players_array[0][2] += 1
     players_array[0][1] = score_calculator(players_array[0][1], lanzamientos);
 
     console.log(`El nuevo puntaje de ${players_array[0][0]} es: ${players_array[0][1]}\n`)
-    recursive_game(score_calculator, turn_comparison, input_function)(players_array) //arreglar con Y
+    recursive_func(players_array) //arreglar con Y
   }
+  // Turno de otro
   else {
-    const n_l = await input_function(`Ingrese lanzamientos de ${not_played[0][0]}:\n`);
+    const n_l = "['SB', [2,20], [3,20]]";
     const lanzamientos = parse_function(n_l);
-    players_array[players_array.indexOf(not_played[0])][1] = score_calculator(players_array[players_array.indexOf(not_played[0])][1], lanzamientos);
+    const player_index = players_array.indexOf(not_played[0]);
+    players_array[player_index][2] += 1;
+    players_array[player_index][1] = score_calculator(players_array[player_index][1], lanzamientos);
 
-    console.log(`El nuevo puntaje de ${not_played[0][0]} es: ${not_played[0][1]}\n`)
-    recursive_game(score_calculator, turn_comparison, input_function)(players_array) //arreglar con Y
+    console.log(`El nuevo puntaje de ${not_played[0][0]} es: ${players_array[player_index][1]}\n`)
+    recursive_func(players_array) //arreglar con Y
   }
 };
 
@@ -113,11 +135,11 @@ const announce = 'Juego inicializado con jugadores';
 console.log('Juego inicializado con jugadores', players.slice(0,-1).toString(), 'y', players.slice(-1).toString())
 
 const players_array = get_info(players);
-recursive_game(new_score, compare_turns, get_input, parse_throw_array)(players_array);
+Y(recursive_game(new_score, compare_turns, parse_throw_array))(players_array);
 
-
+// console.log('despues del recursive game')
 // Juego inicializado con jugadores Jaime y  Ema.  Ingrese lanzamientos de Jaime
-// ['DB', [3,20], [3,19]]
+// ['SB', [2,20], [3,20]]
 // Jaime queda con 334 puntos. Ingrese lanzamientos de Ema
 // ['SB', [2,20], [3,20]]
 // Ema queda con 376 puntos. Ingrese lanzamientos de Jaime
--- prueba_y.js
+++
@@ -1,7 +0,0 @@
-const factorial_gen1 = f => (n => ((n === 0) ? 1 : n * f(n - 1)));
-const factorial_gen2 = (func) => f => (n => ((n === 0) ? 1 : func(n, f(n - 1))));
-const factorial_gen3 = (func) => f => (async n => ((n === 0) ? 1 : func(n, f(n - 1))));
-const mult = (a,b) => (a*b)
-const Y = f => (x => x(x))(x => f(y => x(x)(y)))
-
-console.log(Y(factorial_gen3(mult))(3))
